59f1683e35fd55b50d75a415b88c44924e18143e
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-01-24 18:46:07 -0800

    linked errors solved 2.0

--- Not-Red/Framework/Core/Src/TimeUtil.cpp
+++ Not-Red/Framework/Core/Src/TimeUtil.cpp
@@ -1,22 +1,21 @@
-#pragma once
 #include "Precompiled.h"
 #include "TimeUtil.h"
 
-namespace NotRed::Core
+using namespace NotRed;
+using namespace NotRed::Core;
+
+float TimeUtil::GetTime()
 {
-	float TimeUtil::GetTime()
-	{
-		static const auto startTime = std::chrono::high_resolution_clock::now();
-		const auto currentTime = std::chrono::high_resolution_clock::now();
-		const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
-		return milliseconds / 1000.0f;
-	}
-	float TimeUtil::GetdeltaTime()
-	{
-		static auto lastCallTime = std::chrono::high_resolution_clock::now();
-		const auto currentTime = std::chrono::high_resolution_clock::now();
-		const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastCallTime).count();
-		lastCallTime = currentTime;
-		return milliseconds / 1000.0f;
-	}
+	static const auto startTime = std::chrono::high_resolution_clock::now();
+	const auto currentTime = std::chrono::high_resolution_clock::now();
+	const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
+	return milliseconds / 1000.0f;
+}
+float TimeUtil::GetdeltaTime()
+{
+	static auto lastCallTime = std::chrono::high_resolution_clock::now();
+	const auto currentTime = std::chrono::high_resolution_clock::now();
+	const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastCallTime).count();
+	lastCallTime = currentTime;
+	return milliseconds / 1000.0f;
 }
