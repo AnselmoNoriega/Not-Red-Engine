bdb815d648f3efa2eb9d3391bcdf0d51a4d22371
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-04 12:28:44 -0700

    unfinished stuff

--- NotRed/Tools/ModelImporter/main.cpp
+++ NotRed/Tools/ModelImporter/main.cpp
@@ -32,6 +32,16 @@ Vector2 ToTexCoord(const aiVector3D& v)
     };
 }
 
+Color ToColor(const aiColor3D& color)
+{
+    return{
+        static_cast<float>(color.r),
+        static_cast<float>(color.g),
+        static_cast<float>(color.b),
+        1.0f
+    };
+}
+
 std::optional<Arguments> ParseArgs(int argc, char* argv[])
 {
     if (argc < 3)
@@ -52,6 +62,97 @@ std::optional<Arguments> ParseArgs(int argc, char* argv[])
     }
 
     return args;
+}
+
+void ExportEmbeddedTexture(const aiTexture* texture, const Arguments& args, const std::filesystem::path& fileName)
+{
+    printf("Extracting embedded texture %s\n", fileName.string().c_str());
+
+    std::string fullFileName = args.outputFileName.string();
+    fullFileName = fullFileName.substr(0, fullFileName.rfind('/') + 1);
+    fullFileName += fileName.filename().string();
+
+    FILE* file = nullptr;
+    auto err = fopen_s(&file, fullFileName.c_str(), "wb");
+    if (err != 0 || file == nullptr)
+    {
+        printf("Error: failed to open file %s!\n", fullFileName.c_str());
+        return;
+    }
+
+    size_t written = fwrite(texture->pcData, 1, texture->mWidth, file);////////////////////////
+    ASSERT(written = texture->mWidth, "ERROR");
+}
+
+std::string FindTexture(const aiScene* scene, const aiMaterial* aiMaterial,
+    aiTextureType textureType, const Arguments& args, const std::string& suffix,
+    uint32_t materialIndex
+)
+{
+    const uint32_t textureCount = aiMaterial->GetTextureCount(textureType);
+    if (textureCount == 0)
+    {
+        return "";
+    }
+
+    std::filesystem::path textureName;
+    aiString texturePath;
+    if (aiMaterial->GetTexture(textureType, 0, &texturePath) == aiReturn_SUCCESS)
+    {
+        if (texturePath.C_Str()[0] == '*')
+        {
+            std::string fileName = args.inputFileName.string();
+            fileName.erase(fileName.length() - 4);
+            fileName += suffix;
+            fileName += texturePath.C_Str()[1];
+
+            ASSERT(scene->HasTextures(), "Error: no embedded texture found!");
+
+            int textureIndex = atoi(texturePath.C_Str() + 1);
+            ASSERT(textureIndex < scene->mNumTextures, "Error: invalid texture index!");
+
+            const aiTexture* embeddedTexture = scene->mTextures[textureIndex];
+            ASSERT(embeddedTexture->mHeight == 0, "Error: uncompressed texture found!");
+
+            if (embeddedTexture->CheckFormat("jpg"))
+            {
+                fileName += ".jpg";
+            }
+            else if (embeddedTexture->CheckFormat("png"))
+            {
+                fileName += ".png";
+            }
+            else
+            {
+                ASSERT(false, "Error: unrecognized texture format!");
+            }
+
+            ExportEmbeddedTexture(embeddedTexture, args, fileName);
+            printf("Adding textuer s%...\n", textureName.c_str());
+            textureName = fileName;
+        }
+        else if (auto embeddedTeture = scene->GetEmbeddedTexture(texturePath.C_Str()); embeddedTeture)
+        {
+            std::filesystem::path embeddedFilePath = texturePath.C_Str();
+            std::string fileName = args.inputFileName.string();
+            fileName.erase(fileName.length() - 4);
+            fileName += suffix;
+            fileName += "_" + std::to_string(materialIndex);
+            fileName += embeddedFilePath.extension().string();
+
+            ExportEmbeddedTexture(embeddedTeture, args, fileName);
+            printf("Adding textuer s%...\n", textureName.c_str());
+            textureName = fileName;
+        }
+        else
+        {
+            std::filesystem::path embeddedFilePath = texturePath.C_Str();
+            std::string fileName = args.inputFileName.string();
+
+            printf("Adding textuer s%...\n", textureName.c_str());
+            textureName = fileName;
+        }
+    }
 }
 
 int main(int argc, char* argv[])
@@ -127,6 +228,38 @@ int main(int argc, char* argv[])
             }
         }
     }
+    
+    if (scene->HasMaterials())
+    {
+        printf("Reading Material Data...");
+
+        const uint32_t numMaterials = scene->mNumMaterials;
+        model.meshData.reserve(numMaterials);
+        for (uint32_t materialIndex = 0; materialIndex < numMaterials; ++materialIndex)
+        {
+            const aiMaterial* assimpMaterial = scene->mMaterials[materialIndex];
+            aiColor3D ambient, diffuse, emissive, specular;
+            ai_real specularPower = 1.0f;
+
+            assimpMaterial->Get(AI_MATKEY_COLOR_AMBIENT, ambient);
+            assimpMaterial->Get(AI_MATKEY_COLOR_DIFFUSE, diffuse);
+            assimpMaterial->Get(AI_MATKEY_COLOR_EMISSIVE, emissive);
+            assimpMaterial->Get(AI_MATKEY_COLOR_SPECULAR, specular);
+            assimpMaterial->Get(AI_MATKEY_SHININESS, specularPower);
+
+            Model::MeterialData& materialData = model.meterialData.emplace_back();
+            materialData.material.ambient = ToColor(ambient);
+            materialData.material.diffuse = ToColor(diffuse);
+            materialData.material.emissive = ToColor(emissive);
+            materialData.material.specular = ToColor(specular);
+            materialData.material.power = (float)specularPower;
+
+            materialData.diffuseMapName = FindTexture(scene, assimpMaterial, aiTextureType_DIFFUSE, args, "_diff", materialIndex);
+            materialData.normalMapName = FindTexture(scene, assimpMaterial, aiTextureType_NORMALS, args, "_norm", materialIndex);
+            materialData.bumpMapName = FindTexture(scene, assimpMaterial, aiTextureType_DISPLACEMENT, args, "_bump", materialIndex);
+            materialData.specularMapName = FindTexture(scene, assimpMaterial, aiTextureType_SPECULAR, args, "_spec", materialIndex);
+        }
+    }
 
     printf("Saving Model...\n");
     if (ModelIO::SaveModel(args.outputFileName, model))
@@ -136,6 +269,16 @@ int main(int argc, char* argv[])
     else
     {
         printf("Failed to save Model data [%s]...\n", args.outputFileName.string().c_str());
+    }
+
+    printf("Saving Material...\n");
+    if (ModelIO::SaveMaterial(args.outputFileName, model))
+    {
+        printf("Material saved [%s]...\n", args.outputFileName.string().c_str());
+    }
+    else
+    {
+        printf("Failed to save Material data [%s]...\n", args.outputFileName.string().c_str());
     }
 
     return 0;
