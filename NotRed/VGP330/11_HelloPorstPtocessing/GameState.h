00b4473da9e53472c9c6fe4ba52f2a48876dba46
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-11 09:25:55 -0700

    new project

---
+++ NotRed/VGP330/11_HelloPorstPtocessing/GameState.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Not-Red/Inc/NotRed.h>
+
+class MainState : public NotRed::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float dt) override;
+	void Render() override;
+	void DebugUI() override;
+
+private:
+	void CameraControllers(float dt);
+
+private:
+	NotRed::Graphics::DirectionalLight mDirectionalLight;
+	NotRed::Graphics::Camera mCamera;
+
+	NotRed::Graphics::RenderGroup mRenderGroupA;
+	Math::Matrix4 mPositionA;
+	NotRed::Graphics::RenderGroup mRenderGroupB;
+	Math::Matrix4 mPositionB;
+
+	NotRed::Graphics::StandardEffect mStandardEffect;
+};
