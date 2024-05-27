68f8d96afb48f7e9c15b90c8bf30846f5e1e1e64
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-26 18:52:03 -0700

    project init

---
+++ NotRed/VGP330/13_HelloShadows/GameState.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Not-Red/Inc/NotRed.h>
+
+class MainState : public NotRed::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float dt) override;
+	void Render() override;
+	void DebugUI() override;
+
+private:
+	void CameraControllers(float dt);
+
+private:
+	NotRed::Graphics::DirectionalLight mDirectionalLight;
+	NotRed::Graphics::Camera mCamera;
+
+	NotRed::Graphics::StandardEffect mStandardEffect;
+	NotRed::Graphics::PostPricessingEffect mPostPricessingEffect;
+	NotRed::Graphics::GaussianBlur mGaussianBlurEffect;
+
+	NotRed::Graphics::RenderGroup mRenderGroupA;
+	Math::Matrix4 mPositionA;
+	NotRed::Graphics::RenderGroup mRenderGroupB;
+	Math::Matrix4 mPositionB;
+
+	NotRed::Graphics::RenderObject mGround;
+	NotRed::Graphics::RenderObject mScreenQuad;
+
+	NotRed::Graphics::RenderTarget mRenderTarget;
+	NotRed::Graphics::RenderTarget mBlurRenderTarget;
+};
