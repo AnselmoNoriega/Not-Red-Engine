f424e428a6adb6228ad9ec7cea2af3ed1e16f03d
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-07-20 19:03:11 -0700

    skeleton importer

--- NotRed/Framework/Graphics/Inc/Model.h
+++ NotRed/Framework/Graphics/Inc/Model.h
@@ -2,6 +2,7 @@
 
 #include "MeshTypes.h"
 #include "Material.h"
+#include "Skeleton.h"
 
 namespace NotRed::Graphics
 {
@@ -24,5 +25,6 @@ namespace NotRed::Graphics
 
         std::vector<MeshData> meshData;
         std::vector<MeterialData> meterialData;
+        std::unique_ptr<Skeleton> skeleton;
     };
 }
