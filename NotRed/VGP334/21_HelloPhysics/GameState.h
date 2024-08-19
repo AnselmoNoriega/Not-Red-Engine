69fae759f3d4e25d5592a77ae1cb8bcd023f172b
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-08-18 17:09:12 -0700

    game state physics

---
+++ NotRed/VGP334/21_HelloPhysics/GameState.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Not-Red/Inc/NotRed.h>
+
+class MainState : public NotRed::AppState
+{
+public:
+    void Initialize()  override;
+    void Terminate()  override;
+    void Update(const float deltaTime) override;
+    void Render() override;
+    void DebugUI() override;
+
+private:
+    void UpdateCameraControl(float deltaTime);
+
+    NotRed::Graphics::Camera mCamera;
+    NotRed::Graphics::DirectionalLight mDirectionalLight;
+    NotRed::Graphics::StandardEffect mStandardEffect;
+
+    NotRed::Graphics::RenderObject mBall;
+    NotRed::Physics::CollisionShape mBallShape;
+    NotRed::Physics::RigidBody mBallRB;
+
+    NotRed::Graphics::RenderObject mGround;
+    NotRed::Physics::CollisionShape mGroundShape;
+    NotRed::Physics::RigidBody mGroundRB;
+
+    float mPitch = 0.0f;
+    float mYaw = 0.0f;
+    float mRoll = 0.0f;
+};
