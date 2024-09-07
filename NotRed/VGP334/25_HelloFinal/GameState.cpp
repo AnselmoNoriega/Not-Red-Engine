137958b4f11494cbae975da7b06a754db87be61d
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-09-07 14:39:45 -0700

    start project set up

---
+++ NotRed/VGP334/25_HelloFinal/GameState.cpp
@@ -0,0 +1,109 @@
+#include "GameState.h"
+
+using namespace NotRed;
+using namespace NotRed::Graphics;
+using namespace NotRed::Math;
+using namespace NotRed::Input;
+using namespace NotRed::Audio;
+
+void MainState::UpdateCameraControl(float dt)
+{
+	auto input = Input::InputSystem::Get();
+	const float moveSpeed = input->IsKeyDown(KeyCode::LSHIFT) ? 10.0f : 1.0f;
+	const float turnSpeed = 0.8f;
+
+	if (input->IsKeyDown(KeyCode::W))
+	{
+		mCamera.Walk(moveSpeed * dt);
+	}
+	if (input->IsKeyDown(KeyCode::A))
+	{
+		mCamera.Strafe(-moveSpeed * dt);
+	}
+	if (input->IsKeyDown(KeyCode::S))
+	{
+		mCamera.Walk(-moveSpeed * dt);
+	}
+	if (input->IsKeyDown(KeyCode::D))
+	{
+		mCamera.Strafe(moveSpeed * dt);
+	}
+	if (input->IsKeyDown(KeyCode::E))
+	{
+		mCamera.Rise(moveSpeed * dt);
+	}
+	if (input->IsKeyDown(KeyCode::Q))
+	{
+		mCamera.Rise(-moveSpeed * dt);
+	}
+
+	if (input->IsMouseDown(MouseButton::LBUTTON))
+	{
+		mCamera.Yaw(input->GetMouseMoveX() * turnSpeed * dt);
+		mCamera.Pitch(input->GetMouseMoveY() * turnSpeed * dt);
+	}
+
+	if (input->IsKeyPressed(KeyCode::SPACE))
+	{
+		SpacePressedEvent spacePressed;
+		EventManager::Broadcast(&spacePressed);
+	}
+}
+
+void MainState::Initialize()
+{
+	mCamera.SetPosition({ 0.0f,1.0f,-4.0f });
+	mCamera.SetLookAt({ 0.0f,0.0f,0.0f });
+
+	mDirectionalLight.direction = Math::Normalize({ 1.0f,-1.0f,1.0f });
+	mDirectionalLight.ambient = { 0.5f,0.5f,0.5f,1.0f };
+	mDirectionalLight.diffuse = { 0.8f,0.8f,0.8f,1.0f };
+	mDirectionalLight.specular = { 1.0f,1.0f,1.0f,1.0f };
+
+	mStandardEffect.Initialize(L"../../Assets/Shaders/Standard.fx");
+	mStandardEffect.SetCamera(mCamera);
+	mStandardEffect.SetDirectionalLight(mDirectionalLight);
+
+	Model modelA;
+	ModelIO::LoadModel("../../Assets/Models/Mike/Bboy.model", modelA);
+	ModelIO::LoadMaterial("../../Assets/Models/Mike/Bboy.model", modelA);
+	mCharacter = CreateRenderGroup(modelA);
+
+}
+
+void MainState::Terminate()
+{
+	CleanRenderGroup(mCharacter);
+	mStandardEffect.Terminate();
+}
+
+void MainState::Update(const float deltaTime)
+{
+	UpdateCameraControl(deltaTime);
+}
+
+void MainState::Render()
+{
+	mStandardEffect.Begin();
+	{
+		DrawRenderGroup(mStandardEffect, mCharacter);
+	}
+	mStandardEffect.End();
+}
+
+void MainState::DebugUI()
+{
+	ImGui::Begin("Debug control", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	if (ImGui::CollapsingHeader("Light", ImGuiTreeNodeFlags_DefaultOpen))
+	{
+		if (ImGui::DragFloat3("Direction", &mDirectionalLight.direction.x, 0.1f))
+		{
+			mDirectionalLight.direction = Math::Normalize(mDirectionalLight.direction);
+		}
+
+		ImGui::ColorEdit4("Ambient##Light", &mDirectionalLight.ambient.r);
+		ImGui::ColorEdit4("Diffuse##Light", &mDirectionalLight.diffuse.r);
+		ImGui::ColorEdit4("Specular##Light", &mDirectionalLight.specular.r);
+	}
+	ImGui::End();
+}
