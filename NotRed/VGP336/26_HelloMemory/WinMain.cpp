c14abf5efcc96280cdfcada3bab3fd9b66643581
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-10-11 16:44:28 -0700

    deleting winmain

--- NotRed/VGP336/26_HelloMemory/WinMain.cpp
+++ NotRed/VGP336/26_HelloMemory/WinMain.cpp
@@ -1,18 +1,18 @@
 #include <Not-Red/Inc/NotRed.h>
 
-#include "GameState.h"
-
-using namespace NotRed;
-
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
-{
-	App& myApp = NotRed::MainApp();
-	myApp.AddState<MainState>("MainState");
-
-	AppConfig config;
-	config.appName = L"Hello Anim";
-
-	myApp.Run(config);
-
-	return(0);
-}
+//#include "GameState.h"
+//
+//using namespace NotRed;
+//
+//int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
+//{
+//	App& myApp = NotRed::MainApp();
+//	myApp.AddState<MainState>("MainState");
+//
+//	AppConfig config;
+//	config.appName = L"Hello Anim";
+//
+//	myApp.Run(config);
+//
+//	return(0);
+//}
